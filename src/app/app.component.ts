df848b536ebab3cef00011e67dc0f294560f56ae
felipe R <salas@unilibre.edu.co>
2019-08-21 20:25:53 -0500

    Ading it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-dream-app';
+  title = 'Jorge\'s App';
+  var2='Jorge Morales';
+  var3=19;
+  habilitacion=false;
+  nombre:string = '';
+
+  paises:any = [
+    {nombre: 'Colombia'},
+    {nombre: 'Brasil'},
+    {nombre: 'Argentina'},
+    {nombre: 'Polonia'},
+    {nombre: 'Alemania'},
+    {nombre: 'Francia'},
+  ]
+
+
+  constructor(){
+    this.cons();
+  }
+
+  cons(){
+    this.habilitacion=!this.habilitacion;
+    setTimeout(() => {
+      this.habilitacion = !this.habilitacion;
+    }, 3000)
+  }
+
+  ejecutar(){
+    this.var3=this.var3+1;
+  }
+
+  restart(){
+    this.cons();
+    this.var3=19;
+  }
+
 }
