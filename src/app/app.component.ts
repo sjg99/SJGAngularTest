1968dcba723b9568e3eb5192200deab14c8420ab
sjg99 <38005341+sjg99@users.noreply.github.com>
2019-08-26 20:23:30 -0500

    I just stole yorcht code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,13 +9,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Jorge\'s App';
+  title = 'SJG App';
   lat = 4.607;
   lng = -74.1024516;
-  var2='Jorge Morales';
-  var3=19;
-  habilitacion=false;
-  nombre:string = '';
 
   paises:any = [
     {activo: true, nombre: 'Colombia'},
@@ -25,26 +21,4 @@ export class AppComponent {
     {activo: true, nombre: 'Alemania'},
     {activo: true, nombre: 'Francia'},
   ]
-
-
-  constructor(){
-    this.cons();
-  }
-
-  cons(){
-    this.habilitacion=!this.habilitacion;
-    setTimeout(() => {
-      this.habilitacion = !this.habilitacion;
-    }, 3000)
-  }
-
-  ejecutar(){
-    this.var3=this.var3+1;
-  }
-
-  restart(){
-    this.cons();
-    this.var3=19;
-  }
-
 }
